74255ea78cd93bbab63b05dff9b8d28330409b08
SwetlanaAng <93288183+SwetlanaAng@users.noreply.github.com>
2024-08-05 22:30:40 +0200

    init: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+    let counter = new Map();
+    for (let i=0; i<bracketsConfig.length; i++){
+        counter.set(bracketsConfig[i][0], bracketsConfig[i][1]);}
+    for(let i=0; i<str.length; i++){
+        if(counter.has(str[i])){
+            if(str[i]===counter.get(str[i])){
+                if(arr[arr.length-1] === str[i]) arr.pop();
+                else arr.push(str[i]);
+            } else arr.push(str[i])
+        } else if (counter.get(arr[arr.length-1])===str[i]){
+            arr.pop();
+        }else if(str[i]!='|')return false;
+    }
+    if (arr.length ===0){
+        return true
+    }else return false;
 }
